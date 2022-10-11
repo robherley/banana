5642df558a882ca704b3df2ff6dc1afcaf334169
Rob Herley <robherley@github.com>
2022-10-11 14:32:33 -0400

    read some files

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"os"
 
-	"github.com/jaypipes/ghw"
 	authorizationv1 "k8s.io/api/authorization/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -45,15 +43,6 @@ func k8sSubject() (interface{}, error) {
 	return client.AuthorizationV1().SelfSubjectRulesReviews().Create(context.TODO(), ssrr, metav1.CreateOptions{})
 }
 
-func k8sPods() (interface{}, error) {
-	client, err := k8sClient()
-	if err != nil {
-		return nil, err
-	}
-
-	return client.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
-}
-
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
@@ -63,15 +52,25 @@ func main() {
 
 		errors := []string{}
 
-		var gpus []string
-		gpuInfo, err := ghw.GPU()
+		// var gpus []string
+		// gpuInfo, err := ghw.GPU()
+		// if err != nil {
+		// 	errors = append(errors, fmt.Sprintf("gpus: %s", err))
+		// } else {
+		// 	gpus = make([]string, len(gpuInfo.GraphicsCards))
+		// 	for i := range gpuInfo.GraphicsCards {
+		// 		gpus[i] = gpuInfo.GraphicsCards[i].String()
+		// 	}
+		// }
+
+		saFile, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount")
 		if err != nil {
-			errors = append(errors, fmt.Sprintf("gpus: %s", err))
-		} else {
-			gpus = make([]string, len(gpuInfo.GraphicsCards))
-			for i := range gpuInfo.GraphicsCards {
-				gpus[i] = gpuInfo.GraphicsCards[i].String()
-			}
+			errors = append(errors, err.Error())
+		}
+
+		nsFile, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
+		if err != nil {
+			errors = append(errors, err.Error())
 		}
 
 		// k8sSubject, err := k8sSubject()
@@ -79,17 +78,12 @@ func main() {
 		// 	errors = append(errors, fmt.Sprintf("k8s subject: %s", err))
 		// }
 
-		k8sPods, err := k8sPods()
-		if err != nil {
-			errors = append(errors, fmt.Sprintf("k8s pods: %s", err))
-		}
-
 		response := map[string]interface{}{
-			"gpus":   gpus,
-			"env":    os.Environ(),
+			// "gpus":   gpus,
+			// "env":    os.Environ(),
+			"saFile": string(saFile),
+			"nsFile": string(nsFile),
 			"errors": errors,
-			// "k8sSubject": k8sSubject,
-			"k8sPods": k8sPods,
 		}
 
 		res, err := json.Marshal(response)
