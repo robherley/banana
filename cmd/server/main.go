855edc8282eb3da85fc5fc19535d2a12b647f979
Rob Herley <robherley@github.com>
2022-10-11 00:58:20 -0400

    k8s?

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"os"
 
 	"github.com/jaypipes/ghw"
+	authorizationv1 "k8s.io/api/authorization/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 const (
 	HOST = "0.0.0.0"
 	PORT = "8000"
 )
+
+func k8sInfo() (interface{}, error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	ssrr := &authorizationv1.SelfSubjectRulesReview{
+		Spec: authorizationv1.SelfSubjectRulesReviewSpec{
+			Namespace: "",
+		},
+	}
+
+	return client.AuthorizationV1().SelfSubjectRulesReviews().Create(context.TODO(), ssrr, metav1.CreateOptions{})
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -22,20 +48,29 @@ func main() {
 			return
 		}
 
+		errors := []string{}
+
+		var gpus []string
 		gpuInfo, err := ghw.GPU()
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+			errors = append(errors, fmt.Sprintf("gpus: %s", err))
+		} else {
+			gpus = make([]string, len(gpuInfo.GraphicsCards))
+			for i := range gpuInfo.GraphicsCards {
+				gpus[i] = gpuInfo.GraphicsCards[i].String()
+			}
 		}
 
-		gpus := make([]string, len(gpuInfo.GraphicsCards))
-		for i := range gpuInfo.GraphicsCards {
-			gpus[i] = gpuInfo.GraphicsCards[i].String()
+		k8sSubject, err := k8sInfo()
+		if err != nil {
+			errors = append(errors, fmt.Sprintf("k8s: %s", err))
 		}
 
 		response := map[string]interface{}{
-			"gpus": gpus,
-			"env":  os.Environ(),
+			"gpus":       gpus,
+			"env":        os.Environ(),
+			"errors":     errors,
+			"k8sSubject": k8sSubject,
 		}
 
 		res, err := json.Marshal(response)
